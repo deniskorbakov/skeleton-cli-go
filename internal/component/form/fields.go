7763894bb87b5fab0fdb8bcb8d0fcb42fe62f12e
Denis <korbakovd@gmail.com>
2025-10-03 18:25:11 +0300

    feat: add test field

--- internal/component/form/fields.go
+++ internal/component/form/fields.go
@@ -2,8 +2,5 @@ package form
 
 // Fields A struct that form build repo
 type Fields struct {
-	RepositoryUrl   string
-	RepositoryLocal string
-	RemoteUser      string
-	RemoteHost      string
+	ExampleInput string
 }
